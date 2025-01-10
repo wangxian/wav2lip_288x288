2b63434962dd005c11122ece8f56b62a26dfa1ca
wangxian <xian366@126.com>
2025-01-10 15:21:43 +0800

    test: 训练数据

--- inference.py
+++ inference.py
@@ -1,17 +1,17 @@
-from os import listdir, path
+from os import listdir, path  # noqa
 import numpy as np
-import scipy
+import scipy  # noqa
 import cv2
 import os
-import sys
+import sys  # noqa
 import argparse
 import audio
-import json
+import json  # noqa
 import subprocess
-import random
-import string
+import random  # noqa
+import string  # noqa
 from tqdm import tqdm
-from glob import glob
+from glob import glob  # noqa
 import torch
 import face_detection
 from models import Wav2Lip
@@ -30,8 +30,8 @@ parser.add_argument('--fps', type=float, help='Can be specified only if input is
 
 parser.add_argument('--pads', nargs='+', type=int, default=[0, 10, 0, 0], help='Padding (top, bottom, left, right). Please adjust to include chin at least')
 
-parser.add_argument('--face_det_batch_size', type=int, help='Batch size for face detection', default=16)
-parser.add_argument('--wav2lip_batch_size', type=int, help='Batch size for Wav2Lip model(s)', default=128)
+parser.add_argument('--face_det_batch_size', type=int, help='Batch size for face detection', default=4)  # 16
+parser.add_argument('--wav2lip_batch_size', type=int, help='Batch size for Wav2Lip model(s)', default=16)  # 128
 
 parser.add_argument('--resize_factor', default=1, type=int, help='Reduce the resolution by this factor. Sometimes, best results are obtained at 480p or 720p')
 
@@ -68,21 +68,19 @@ def get_smoothened_boxes(boxes, T):
 
 
 def face_detect(images):
-    detector = face_detection.FaceAlignment(face_detection.LandmarksType._2D,
-                                            flip_input=False, device=device)
-
+    detector = face_detection.FaceAlignment(face_detection.LandmarksType._2D, flip_input=False, device=device)
     batch_size = args.face_det_batch_size
 
     while 1:
         predictions = []
         try:
             for i in tqdm(range(0, len(images), batch_size)):
-                predictions.extend(detector.get_detections_for_batch(
-                    np.array(images[i:i + batch_size])))
+                predictions.extend(detector.get_detections_for_batch(np.array(images[i:i + batch_size])))
+
         except RuntimeError:
             if batch_size == 1:
-                raise RuntimeError(
-                    'Image too big to run face detection on GPU. Please use the --resize_factor argument')
+                raise RuntimeError('Image too big to run face detection on GPU. Please use the --resize_factor argument')
+
             batch_size //= 2
             print('Recovering from OOM error; New batch size: {}'.format(batch_size))
             continue
@@ -180,14 +178,17 @@ def _load(checkpoint_path):
     return checkpoint
 
 
-def load_model(path):
+def load_model(ckp_path):
     model = Wav2Lip()
-    print("Load checkpoint from: {}".format(path))
-    checkpoint = _load(path)
+    print("Load checkpoint from: {}".format(ckp_path))
+    checkpoint = _load(ckp_path)
+
     s = checkpoint["state_dict"]
     new_s = {}
+
     for k, v in s.items():
         new_s[k.replace('module.', '')] = v
+
     model.load_state_dict(new_s)
 
     model = model.to(device)
@@ -196,8 +197,7 @@ def load_model(path):
 
 def main():
     if not os.path.isfile(args.face):
-        raise ValueError(
-            '--face argument must be a valid path to video/image file')
+        raise ValueError('--face argument must be a valid path to video/image file')
 
     elif args.face.split('.')[1] in ['jpg', 'png', 'jpeg']:
         full_frames = [cv2.imread(args.face)]
@@ -215,9 +215,9 @@ def main():
             if not still_reading:
                 video_stream.release()
                 break
+
             if args.resize_factor > 1:
-                frame = cv2.resize(
-                    frame, (frame.shape[1]//args.resize_factor, frame.shape[0]//args.resize_factor))
+                frame = cv2.resize(frame, (frame.shape[1]//args.resize_factor, frame.shape[0]//args.resize_factor))
 
             if args.rotate:
                 frame = cv2.rotate(frame, cv2.cv2.ROTATE_90_CLOCKWISE)
@@ -225,6 +225,7 @@ def main():
             y1, y2, x1, x2 = args.crop
             if x2 == -1:
                 x2 = frame.shape[1]
+
             if y2 == -1:
                 y2 = frame.shape[0]
 
@@ -236,8 +237,7 @@ def main():
 
     if not args.audio.endswith('.wav'):
         print('Extracting raw audio...')
-        command = 'ffmpeg -y -i {} -strict -2 {}'.format(
-            args.audio, 'temp/temp.wav')
+        command = 'ffmpeg -y -i {} -strict -2 {}'.format(args.audio, 'temp/temp.wav')
 
         subprocess.call(command, shell=True)
         args.audio = 'temp/temp.wav'
@@ -247,17 +247,18 @@ def main():
     print(mel.shape)
 
     if np.isnan(mel.reshape(-1)).sum() > 0:
-        raise ValueError(
-            'Mel contains nan! Using a TTS voice? Add a small epsilon noise to the wav file and try again')
+        raise ValueError('Mel contains nan! Using a TTS voice? Add a small epsilon noise to the wav file and try again')
 
     mel_chunks = []
     mel_idx_multiplier = 80./fps
     i = 0
+
     while 1:
         start_idx = int(i * mel_idx_multiplier)
         if start_idx + mel_step_size > len(mel[0]):
             mel_chunks.append(mel[:, len(mel[0]) - mel_step_size:])
             break
+
         mel_chunks.append(mel[:, start_idx: start_idx + mel_step_size])
         i += 1
 
@@ -268,20 +269,16 @@ def main():
     batch_size = args.wav2lip_batch_size
     gen = datagen(full_frames.copy(), mel_chunks)
 
-    for i, (img_batch, mel_batch, frames, coords) in enumerate(tqdm(gen,
-                                                                    total=int(np.ceil(float(len(mel_chunks))/batch_size)))):
+    for i, (img_batch, mel_batch, frames, coords) in enumerate(tqdm(gen, total=int(np.ceil(float(len(mel_chunks))/batch_size)))):
         if i == 0:
             model = load_model(args.checkpoint_path)
             print("Model loaded")
 
             frame_h, frame_w = full_frames[0].shape[:-1]
-            out = cv2.VideoWriter('temp/result.avi',
-                                  cv2.VideoWriter_fourcc(*'DIVX'), fps, (frame_w, frame_h))
+            out = cv2.VideoWriter('temp/result.avi', cv2.VideoWriter_fourcc(*'DIVX'), fps, (frame_w, frame_h))
 
-        img_batch = torch.FloatTensor(
-            np.transpose(img_batch, (0, 3, 1, 2))).to(device)
-        mel_batch = torch.FloatTensor(
-            np.transpose(mel_batch, (0, 3, 1, 2))).to(device)
+        img_batch = torch.FloatTensor(np.transpose(img_batch, (0, 3, 1, 2))).to(device)
+        mel_batch = torch.FloatTensor(np.transpose(mel_batch, (0, 3, 1, 2))).to(device)
 
         with torch.no_grad():
             pred = model(mel_batch, img_batch)
@@ -297,8 +294,7 @@ def main():
 
     out.release()
 
-    command = 'ffmpeg -y -i {} -i {} -strict -2 -q:v 1 {}'.format(
-        args.audio, 'temp/result.avi', args.outfile)
+    command = 'ffmpeg -y -i {} -i {} -strict -2 -q:v 1 {}'.format(args.audio, 'temp/result.avi', args.outfile)
     subprocess.call(command, shell=platform.system() != 'Windows')
 
 
